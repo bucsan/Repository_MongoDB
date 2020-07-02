4e4ac027117ba8fe9867699bcc78bf88c2c33b5e
Thiago Adalberto Paranhos Bucsan <thiago.bucsan@hotmail.com>
2020-07-02 00:16:10 -0300

    Add project files.

---
+++ MongoDB.GenericRepository/Startup.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using MongoDB.GenericRepository.Context;
+using MongoDB.GenericRepository.Interfaces;
+using MongoDB.GenericRepository.Persistence;
+using MongoDB.GenericRepository.Repository;
+using MongoDB.GenericRepository.UoW;
+using Swashbuckle.AspNetCore.Swagger;
+
+namespace MongoDB.GenericRepository
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            // Configure the persistence in another layer
+            MongoDbPersistence.Configure();
+            services.AddSwaggerGen(s =>
+            {
+                s.SwaggerDoc("v1", new Info
+                {
+                    Version = "v1",
+                    Title = "MongoDB Repository",
+                    Description = "Swagger",
+                    Contact = new Contact { Name = "", Email = "", Url = "" },
+                    License = new License { Name = "", Url = "" },
+
+                });
+            });
+            RegisterServices(services);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseMvc();
+            app.UseSwagger(c =>
+            {
+                c.RouteTemplate = "swagger/{documentName}/swagger.json";
+            });
+            app.UseSwaggerUI(s =>
+            {
+                s.SwaggerEndpoint("/swagger/v1/swagger.json", "Repository API v1.0");
+            });
+        }
+
+        private void RegisterServices(IServiceCollection services)
+        {
+            services.AddScoped<IMongoContext, MongoContext>();
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
+            services.AddScoped<IProductRepository, ProductRepository>();
+        }
+    }
+}
